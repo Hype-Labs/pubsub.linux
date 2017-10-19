407c27cb1894ffedec0b5f58962e706ca89e759d
Xavier Araújo <xa@hypelabs.io>
2017-10-19 15:24:59 +0100

    Change the names in the source files to be according to the header files

--- src/hype_pub_sub/network.c
+++ src/hype_pub_sub/network.c
@@ -1,27 +1,27 @@
 
 #include "hype_pub_sub/network.h"
 
-NetworkManager *hype_pub_sub_network_manager_create()
+NetworkManager *hype_pub_sub_network_create()
 {
     return NULL;
 }
 
-char *hype_pub_sub_network_manager_get_service_manager_id(char service_key[])
+char *hype_pub_sub_network_get_service_manager_id(char service_key[])
 {
     return NULL;
 }
 
-int hype_pub_sub_network_manager_update_clients()
+int hype_pub_sub_network_update_clients()
 {
     return 0;
 }
 
-bool hype_pub_sub_network_manager_is_client_online(char client_id[])
+bool hype_pub_sub_network_is_client_online(char client_id[])
 {
     return false;
 }
 
-char *hype_pub_sub_network_manager_get_own_client_id()
+char *hype_pub_sub_network_get_own_client_id()
 {
     return NULL;
 }
