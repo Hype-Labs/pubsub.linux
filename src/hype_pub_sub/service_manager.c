1d4c36f34c5b10ff76050407a052101cd295aa07
Xavier Araújo <xa@hypelabs.io>
2017-10-21 19:58:36 +0100

    Implemented methods of the service manager

--- src/hype_pub_sub/service_manager.c
+++ src/hype_pub_sub/service_manager.c
@@ -6,20 +6,33 @@ ServiceManager *hype_pub_sub_service_manager_create(byte service_key[SHA1_BLOCK_
     ServiceManager* servMan = (ServiceManager*) malloc(sizeof(ServiceManager));
     memcpy(servMan->service_key, service_key, SHA1_BLOCK_SIZE * sizeof(byte));
 
+    servMan->subscribers = hype_pub_sub_subscriber_list_create();
+
     return servMan;
 }
 
 int hype_pub_sub_service_manager_add_subscriber(ServiceManager *serv_man, byte subscriber_id[])
 {
+    if(serv_man == NULL)
+        return -1;
+
+    hype_pub_sub_subscriber_list_add(serv_man->subscribers, subscriber_id);
     return 0;
 }
 
 int hype_pub_sub_service_manager_remove_subscriber(ServiceManager *serv_man, byte subscriber_id[])
 {
+    if(serv_man == NULL)
+        return -1;
+
+    hype_pub_sub_subscriber_list_remove(&(serv_man->subscribers), subscriber_id);
     return 0;
 }
 
 bool hype_pub_sub_service_manager_is_subscriber(ServiceManager *serv_man, byte subscriber_id[])
 {
-    return 0;
+    if(serv_man == NULL)
+        return false;
+
+    return hype_pub_sub_subscriber_list_is_subscribed(serv_man->subscribers, subscriber_id);
 }
