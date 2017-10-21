a9f8c4ffd8a18817197e6769ecbed49df0960dfa
Xavier Araújo <xa@hypelabs.io>
2017-10-21 20:25:26 +0100

    Added code to test service_manager.

--- src/hype_pub_sub/main.c
+++ src/hype_pub_sub/main.c
@@ -5,6 +5,9 @@
 #include "hype_pub_sub/hype_pub_sub.h"
 #include "../external/include/sha/sha1.h"
 
+void test_subscribers_list();
+void test_service_manager();
+
 void print_hex_char_array(unsigned char* array, size_t len);
 void print_subs_list(Subscriber* head);
 
@@ -25,6 +28,15 @@ int main()
     printf("Service name: %s\n", hSubs->service_name);
     print_hex_char_array(hSubs->service_key, SHA1_BLOCK_SIZE);
 
+    //test_subscribers_list();
+
+    test_service_manager();
+
+    return 0;
+}
+
+void test_subscribers_list()
+{
     Subscriber* subscribers = hype_pub_sub_subscriber_list_create();
     hype_pub_sub_subscriber_list_add(subscribers, EXAMPLE_ID1);
     hype_pub_sub_subscriber_list_add(subscribers, EXAMPLE_ID2);
@@ -63,12 +75,32 @@ int main()
     hype_pub_sub_subscriber_list_add(subscribers, EXAMPLE_ID3);
     hype_pub_sub_subscriber_list_add(subscribers, EXAMPLE_ID4);
     print_subs_list(subscribers);
-
-    //ServiceManager* hServMan = hype_pub_sub_service_manager_create(sha1_digest(service));
-
-    return 0;
 }
 
+
+void test_service_manager()
+{
+    char service[] = {"HypeCoffe"};
+    byte servKey[SHA1_BLOCK_SIZE];
+    sha1_digest((const byte*) service, strlen(service), servKey);
+
+    ServiceManager* servMan = hype_pub_sub_service_manager_create(servKey);
+
+    printf("\nAdded servMan id2\n");
+    hype_pub_sub_service_manager_add_subscriber(servMan,EXAMPLE_ID2);
+    print_subs_list(servMan->subscribers);
+
+    printf("\nAdded servMan id2\n");
+    hype_pub_sub_service_manager_add_subscriber(servMan,EXAMPLE_ID2);
+    print_subs_list(servMan->subscribers);
+
+
+    printf("\nAdded servMan id1, id3\n");
+    hype_pub_sub_service_manager_add_subscriber(servMan,EXAMPLE_ID1);
+    hype_pub_sub_service_manager_add_subscriber(servMan,EXAMPLE_ID3);
+    print_subs_list(servMan->subscribers);
+
+}
 
 void print_hex_char_array(unsigned char* array, size_t len)
 {
