46cce2889cfa23be3637bccd4483de675b57ff30
Xavier Araújo <xa@hypelabs.io>
2017-10-26 10:42:25 +0100

    Modifications to the CMakeLists to also include the tests. Added main test file

---
+++ test/src/hype_pub_sub/test_main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+
+#include <CUnit/Basic.h>
+
+
+
+void test1(void)
+{
+
+}
+
+void test2()
+{
+
+}
+
+int main()
+{
+   CU_pSuite pSuite = NULL;
+
+   if (CUE_SUCCESS != CU_initialize_registry())
+      return CU_get_error();
+
+   pSuite = CU_add_suite("HypePubSub_TestSuite", NULL, NULL);
+
+   if (pSuite == NULL)
+   {
+      CU_cleanup_registry();
+      return CU_get_error();
+   }
+
+   if ((CU_add_test(pSuite, "Test 1", test1) == NULL) ||
+       (CU_add_test(pSuite, "Test 2", test2) == NULL))
+   {
+      CU_cleanup_registry();
+      return CU_get_error();
+   }
+
+   CU_basic_set_mode(CU_BRM_VERBOSE);
+   CU_basic_run_tests();
+   CU_cleanup_registry();
+   return CU_get_error();
+}
