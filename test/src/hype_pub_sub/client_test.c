b7b8fd9a2ac2c8c0bb1a55620c191b992e729c80
Xavier Araújo <xa@hypelabs.io>
2017-10-28 21:39:30 +0100

    Finished tests of Client.c

--- test/src/hype_pub_sub/client_test.c
+++ test/src/hype_pub_sub/client_test.c
@@ -3,9 +3,9 @@
 
 void hype_pub_sub_client_test()
 {
-    byte CLIENT1_ID[] = "\x85\xa9\xd4\xc4\xde\xd2\x87\x75\x0f\xc0\xed\x32";
-    byte CLIENT2_ID[] = "\x66\xd8\xf2\x20\x6a\x56\xdb\xe9\x91\x23\x3b\xc2";
-    byte CLIENT3_ID[] = "\xe7\x79\x34\x6c\x66\x9c\x17\xf4\x34\xc8\xce\x0e";
+    byte CLIENT1_ID[] = {0x85, 0xa9, 0xd4, 0xc4, 0xde ,0xd2, 0x87 ,0x75, 0x0f, 0xc0, 0xed, 0x32};
+    byte CLIENT2_ID[] = {0x66, 0xd8, 0xf2, 0x20, 0x6a, 0x56, 0xdb, 0xe9, 0x91, 0x23, 0x3b, 0xc2};
+    byte CLIENT3_ID[] = {0xe7, 0x79, 0x34, 0x6c, 0x66, 0x9c, 0x17, 0xf4, 0x34, 0xc8, 0xce, 0x0e};
 
     Client *cl1 = hype_pub_sub_client_create(CLIENT1_ID);
     Client *cl2 = hype_pub_sub_client_create(CLIENT2_ID);
@@ -15,18 +15,9 @@ void hype_pub_sub_client_test()
     CU_ASSERT_NSTRING_EQUAL(cl2->id, "\x66\xd8\xf2\x20\x6a\x56\xdb\xe9\x91\x23\x3b\xc2", HYPE_CONSTANTS_ID_BYTE_SIZE);
     CU_ASSERT_NSTRING_EQUAL(cl3->id, "\xe7\x79\x34\x6c\x66\x9c\x17\xf4\x34\xc8\xce\x0e", HYPE_CONSTANTS_ID_BYTE_SIZE);
 
-    /*
-    CU_ASSERT_NSTRING_EQUAL(cl1->key, "\x05\x2c\x5f\x25\xd0\xee\x44\x4c\x1a\xad\xcc\x48\x02\x89\xc8\x02\xb9\xf0\x78\x89", SHA1_BLOCK_SIZE);
-    CU_ASSERT_NSTRING_EQUAL(cl1->key, "\xd3\xff\x03\x3e\x44\x9e\xd4\x2f\xd7\x7c\xf3\x37\x45\x6b\xb4\x2d\xa4\x10\x9b\xb0", SHA1_BLOCK_SIZE);
-
-    for(int i=0; i<SHA1_BLOCK_SIZE;i++)
-    {
-        printf("%x\n", cl1->key[i]);
-    }
-
-    //CU_ASSERT_NSTRING_EQUAL(cl2->key, "", SHA1_BLOCK_SIZE);
-    //CU_ASSERT_NSTRING_EQUAL(cl3->key, "", SHA1_BLOCK_SIZE);
-    */
+    CU_ASSERT_NSTRING_EQUAL(cl1->key, "\x05\xeb\x63\x7c\xbd\x3f\x33\x69\x1d\x74\x3c\x2a\x39\xaf\xee\xda\x5e\xc9\x45\xad", SHA1_BLOCK_SIZE);
+    CU_ASSERT_NSTRING_EQUAL(cl2->key, "\xf6\xcb\x6d\x9d\xb0\x98\x91\x9b\x2d\x39\x55\x11\x41\xc5\xcb\xe7\x67\xb5\x06\xd6", SHA1_BLOCK_SIZE);
+    CU_ASSERT_NSTRING_EQUAL(cl3->key, "\xe4\x9a\xa7\x79\x2c\xf4\xfd\x09\x6c\x10\x3f\x4b\xa4\x63\xe2\x7b\x91\x60\x9e\x6b", SHA1_BLOCK_SIZE);
 
     Client *cl4 = hype_pub_sub_client_create(CLIENT1_ID);
     CU_ASSERT_FALSE(hype_pub_sub_client_is_id_equal(cl1->id, cl2->id));
@@ -35,7 +26,9 @@ void hype_pub_sub_client_test()
     hype_pub_sub_client_destroy(&cl1);
     hype_pub_sub_client_destroy(&cl2);
     hype_pub_sub_client_destroy(&cl3);
+    hype_pub_sub_client_destroy(&cl4);
     CU_ASSERT_PTR_NULL(cl1);
     CU_ASSERT_PTR_NULL(cl2);
     CU_ASSERT_PTR_NULL(cl3);
+    CU_ASSERT_PTR_NULL(cl4);
 }
