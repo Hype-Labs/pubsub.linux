de5f2404a2068e82a7e8fbc038d3a3a4b509a785
Xavier Araújo <xa@hypelabs.io>
2017-10-29 20:17:27 +0000

    Fixed issue discovered with valgrind

--- src/hype_pub_sub/subscription.c
+++ src/hype_pub_sub/subscription.c
@@ -5,8 +5,8 @@ Subscription *hpb_subscription_create(char *serv_name, size_t serv_name_len, byt
 {
     Subscription *subs = (Subscription *) malloc(sizeof(Subscription));
 
-    subs->service_name = (char *) malloc (serv_name_len * sizeof(char));
-    strcpy(subs->service_name, (const char*) serv_name);
+    subs->service_name = (char *) malloc ((serv_name_len + 1) * sizeof(char)); // +1 -> \0
+    strncpy(subs->service_name, (const char*) serv_name, serv_name_len);
     sha1_digest((const BYTE *) serv_name, serv_name_len, subs->service_key);
     memcpy(subs->manager_id, man_id, HPB_ID_BYTE_SIZE * sizeof(byte));
 
