784210641740e4621225642a2bd0b48503bcba1e
Xavier Araújo <xa@hypelabs.io>
2017-10-31 08:00:21 +0000

    Added tests for list service manager

--- test/src/hype_pub_sub/list_service_managers_test.c
+++ test/src/hype_pub_sub/list_service_managers_test.c
@@ -2,5 +2,58 @@
 
 void hpb_list_service_managers_test()
 {
+    byte SERVICE_KEY1[] = "\xfe\xb5\xc6\xae\x8a\xb9\x7a\xdf\x53\xf8\xbc\x92\xe5\x51\x69\x82\xb6\x20\x0e\xa4";
+    byte SERVICE_KEY2[] = "\x24\x62\xc4\x5a\x65\xd5\x91\x31\x86\xc9\xb3\x10\xa6\x90\x91\x64\xf5\x5e\xf6\x77";
+    byte SERVICE_KEY3[] = "\x86\xc9\xb3\x10\xa6\x90\x91\x64\xf5\x5e\xf6\x77\x24\x62\xc4\x5a\x65\xd5\x91\x31";
 
+    ServiceManager *aux_ser_manv;
+    ListServiceManagers *serv_managers = hpb_list_service_managers_create();
+
+    CU_ASSERT_PTR_NOT_NULL_FATAL(serv_managers);
+    CU_ASSERT_PTR_NULL(serv_managers->head);
+    CU_ASSERT(serv_managers->size == 0);
+
+    hpb_list_service_managers_add(serv_managers, SERVICE_KEY2);
+    hpb_list_service_managers_add(serv_managers, SERVICE_KEY1);
+    hpb_list_service_managers_add(serv_managers, SERVICE_KEY3);
+
+    LinkedListIterator *it = linked_list_iterator_create(serv_managers);
+
+    aux_ser_manv = (ServiceManager *) linked_list_iterator_get_element(it);
+    CU_ASSERT_NSTRING_EQUAL(aux_ser_manv->service_key, SERVICE_KEY2, SHA1_BLOCK_SIZE);
+    linked_list_iterator_advance(it);
+    aux_ser_manv = (ServiceManager *) linked_list_iterator_get_element(it);
+    CU_ASSERT_NSTRING_EQUAL(aux_ser_manv->service_key, SERVICE_KEY1, SHA1_BLOCK_SIZE);
+    linked_list_iterator_advance(it);
+    aux_ser_manv = (ServiceManager *) linked_list_iterator_get_element(it);
+    CU_ASSERT_NSTRING_EQUAL(aux_ser_manv->service_key, SERVICE_KEY3, SHA1_BLOCK_SIZE);
+    CU_ASSERT_PTR_NOT_NULL(serv_managers->head);
+    CU_ASSERT(serv_managers->size == 3);
+
+    CU_ASSERT(hpb_list_service_managers_find(serv_managers, SERVICE_KEY1) != NULL);
+    CU_ASSERT(hpb_list_service_managers_find(serv_managers, SERVICE_KEY2) != NULL);
+    CU_ASSERT(hpb_list_service_managers_find(serv_managers, SERVICE_KEY3) != NULL);
+    byte NEW_KEY[] = "\x86\xc9\xb3\x10\\x77\x24\x62\xc4xa6\x90\x91\x64\xf5\x5e\xf6\x5a\x65\xd5\x91\x31";
+    CU_ASSERT(hpb_list_service_managers_find(serv_managers, NEW_KEY) == NULL);
+
+    hpb_list_service_managers_remove(serv_managers, SERVICE_KEY1);
+    linked_list_iterator_reset(it);
+    aux_ser_manv = (ServiceManager *) linked_list_iterator_get_element(it);
+    CU_ASSERT_NSTRING_EQUAL(aux_ser_manv->service_key, SERVICE_KEY2, SHA1_BLOCK_SIZE);
+    linked_list_iterator_advance(it);
+    aux_ser_manv = (ServiceManager *) linked_list_iterator_get_element(it);
+    CU_ASSERT_NSTRING_EQUAL(aux_ser_manv->service_key, SERVICE_KEY3, SHA1_BLOCK_SIZE);
+    CU_ASSERT(serv_managers->size == 2);
+
+    hpb_list_service_managers_remove(serv_managers, SERVICE_KEY2);
+    linked_list_iterator_reset(it);
+    aux_ser_manv = (ServiceManager *) linked_list_iterator_get_element(it);
+    CU_ASSERT_NSTRING_EQUAL(aux_ser_manv->service_key, SERVICE_KEY3, SHA1_BLOCK_SIZE);
+    CU_ASSERT(serv_managers->size == 1);
+
+    hpb_list_service_managers_remove(serv_managers, SERVICE_KEY3);
+    CU_ASSERT(serv_managers->size == 0);
+
+    hpb_list_service_managers_destroy(&serv_managers);
+    CU_ASSERT_PTR_NULL(serv_managers);
 }
