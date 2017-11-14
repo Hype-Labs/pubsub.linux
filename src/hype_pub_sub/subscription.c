68333b217be384aa72f5faae60797a79f840c449
Xavier Araújo <xa@hypelabs.io>
2017-11-14 11:24:05 +0000

    Fixed possible issue found with Valgrind when allocating space for a string

--- src/hype_pub_sub/subscription.c
+++ src/hype_pub_sub/subscription.c
@@ -5,7 +5,8 @@ HpbSubscription *hpb_subscription_create(char *serv_name, size_t serv_name_len, 
 {
     HpbSubscription *subs = (HpbSubscription *) malloc(sizeof(HpbSubscription));
 
-    subs->service_name = (char *) malloc ((serv_name_len + 1) * sizeof(char)); // +1 -> \0
+    // serv_name_len+1 to consider \0
+    subs->service_name = (char *) calloc ((serv_name_len + 1), sizeof(char));
     strncpy(subs->service_name, (const char*) serv_name, serv_name_len);
     sha1_digest((const BYTE *) serv_name, serv_name_len, subs->service_key);
     memcpy(subs->manager_id, man_id, HPB_ID_BYTE_SIZE * sizeof(HLByte));
