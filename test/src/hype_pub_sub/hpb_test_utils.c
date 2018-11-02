56291c924a3c558d4280fd8a9589d72c9a2acab5
Xavier Araújo <xa@hypelabs.io>
2018-11-02 16:49:14 +0000

    test/include/hype_pub_sub/hpb_test_utils.h~
    added method in test utils to create HypeInstance from a ID byte array. Simplified clients_list tests

--- test/src/hype_pub_sub/hpb_test_utils.c
+++ test/src/hype_pub_sub/hpb_test_utils.c
@@ -2,12 +2,19 @@
 #include <hpb_test_utils.h>
 #include <hype/hype.h>
 
-HpbClient * hpb_test_utils_get_client_from_id(HLByte *id, size_t size)
+
+HypeInstance * hpb_test_utils_get_instance_from_id(HLByte *id, size_t size)
 {
     HypeBuffer *buffer_id = hype_buffer_create_from(id, size);
     HypeInstance *instance = hype_instance_create(buffer_id, NULL, false);
-    HpbClient *client = hpb_client_create(instance);
     hype_buffer_release(buffer_id);
+    return instance;
+}
+
+HpbClient * hpb_test_utils_get_client_from_id(HLByte *id, size_t size)
+{
+    HypeInstance *instance = hpb_test_utils_get_instance_from_id(id, size);
+    HpbClient *client = hpb_client_create(instance);
     hype_instance_release(instance);
     return client;
 }
