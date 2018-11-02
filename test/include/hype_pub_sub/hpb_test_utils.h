4671a8ba58dde63094edb2ae01fca717c1a1a2f4
Xavier Araújo <xa@hypelabs.io>
2018-11-02 16:50:14 +0000

    added function signature to header file

--- test/include/hype_pub_sub/hpb_test_utils.h
+++ test/include/hype_pub_sub/hpb_test_utils.h
@@ -7,6 +7,8 @@
 
 #define HPB_UTILS_CLIENT_ID_TEST_SIZE 12
 
+HypeInstance * hpb_test_utils_get_instance_from_id(HLByte *id, size_t size);
+
 HpbClient * hpb_test_utils_get_client_from_id(HLByte *id, size_t size);
 
 #endif /* HPB_TEST_UTILS_H_INCLUDED_ */
